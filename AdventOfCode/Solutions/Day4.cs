b578318583b1176668c9427d435eef3ba87bfc8d
Robin Krom <robin.krom@gmail.com>
2020-12-09 23:59:30 +0100

    Added the solution for part two

--- AdventOfCode/Solutions/Day4.cs
+++ AdventOfCode/Solutions/Day4.cs
@@ -19,7 +19,6 @@ namespace AdventOfCode.Solutions
             }
             var passport = Regex
                         .Matches(currentPassport, @"([a-zA-Z0-9#]+):([a-zA-Z0-9#]+)\s+")
-                        .Cast<Match>()
                         .Select(m => (Key: m.Groups[1].Value, Value: m.Groups[2].Value))
                         .ToDictionary(kv => kv.Key, k => k.Value);
             _passports.Add(passport);
@@ -31,32 +30,127 @@ namespace AdventOfCode.Solutions
 
             // Read file into maze
             var passportFile = File.ReadAllLines(this.InputFilename);
-            var currentPassport = "";
+            var passportString = "";
             foreach(var passportLine in passportFile)
             {
-                if (!string.IsNullOrEmpty(passportLine))
+                if (!string.IsNullOrWhiteSpace(passportLine))
                 {
-                    currentPassport += passportLine + " ";
+                    passportString += passportLine + " ";
                 }
                 else
                 {
-                    AddPassport(currentPassport);
-                    currentPassport = "";
+                    AddPassport(passportString);
+                    passportString = "";
                 }
             }
             // Process the last
-            AddPassport(currentPassport);
+            AddPassport(passportString);
         }
+
+        private readonly Func<Dictionary<string,string>, bool> _isValidPassportSimple = p => p.Count == 8 || (p.Count == 7 && !p.ContainsKey("cid"));
+
+        private readonly Func<Dictionary<string, string>, bool> _isValidPassport = p => {
+            foreach (var key in p.Keys)
+            {
+                var value = p[key];
+                switch (key)
+                {
+                    case "byr":
+                        // Birth Year - four digits; at least 1920 and at most 2002.
+                        var birthYear = int.Parse(value);
+                        if (birthYear >= 1920 && birthYear <= 2002)
+                        {
+                            break;
+                        }
+                        return false;
+                    case "iyr":
+                        // Issue Year - four digits; at least 2010 and at most 2020.
+                        var issueYear = int.Parse(value);
+                        if (issueYear >= 2010 && issueYear <= 2020)
+                        {
+                            break;
+                        }
+                        return false;
+                    case "eyr":
+                        // (Expiration Year) - four digits; at least 2020 and at most 2030.
+                        var expirationYear = int.Parse(value);
+                        if (expirationYear >= 2020 && expirationYear <= 2030)
+                        {
+                            break;
+                        }
+                        return false;
+                    case "hgt":
+                        // (Height) - a number followed by either cm or in:
+                        // If cm, the number must be at least 150 and at most 193.
+                        // If in, the number must be at least 59 and at most 76.
+                        var match = Regex.Matches(value, @"^(\d+)(cm|in)$");
+                        if (match.Count != 1)
+                        {
+                            return false;
+                        }
+                        var height = int.Parse(match[0].Groups[1].Value);
+
+                        switch (match[0].Groups[2].Value)
+                        {
+                            case "in":
+                                if (height >= 59 && height <= 76)
+                                {
+                                    break;
+                                }
+                                return false;
+                            case "cm":
+                                if (height >= 150 && height <= 193)
+                                {
+                                    break;
+                                }
+                                return false;
+                            default:
+                                return false;
+                        }
+                        break;
+                    case "hcl":
+                        //(Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+                        if (Regex.IsMatch(value, @"^#[a-f0-9]{6}$"))
+                        {
+                            break;
+                        }
+                        return false;
+                    case "ecl":
+                        // (Eye Color) - exactly one of: amb blu brn gry grn hzl oth
+                        if (p[key].Length == 3 && "amb,blu,brn,gry,grn,hzl,oth".IndexOf(value, StringComparison.Ordinal) >= 0)
+                        {
+                            break;
+                        }
+                        return false;
+                    case "pid":
+                        // (Passport ID) - a nine-digit number, including leading zeroes.
+                        if (Regex.IsMatch(value, @"^\d{9}$"))
+                        {
+                            break;
+                        }
+                        return false;
+                    case "cid":
+                        break;
+                    default:
+                        return false;
+                }
+            }
+            if (p.Count < 7 || (p.Count == 7 && p.ContainsKey("cid")))
+            {
+                return false;
+            }
+            return true;
+        };
 
         public override string AnswerPartOne()
         {
-            var answer = _passports.Count(p => p.Count == 8 || (p.Count == 7 && !p.ContainsKey("cid")));
+            var answer = _passports.Where(_isValidPassportSimple).Count();
             return $"{answer}";
         }
 
         public override string AnswerPartTwo()
         {
-            int answer = 0;
+            var answer = _passports.Where(_isValidPassport).Count();
             return $"{answer}";
         }
 
